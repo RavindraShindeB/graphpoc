948ca445c071f14a1f284349c9ff1c5c9cc46ea9
RavindraShindeB <106810973+RavindraShindeB@users.noreply.github.com>
2022-06-07 11:34:40 +0530

    Update GraphController.java

--- src/main/java/com/org/test/microsoftgraphpoc/GraphController.java
+++ src/main/java/com/org/test/microsoftgraphpoc/GraphController.java
@@ -20,6 +20,12 @@ import com.microsoft.graph.requests.GraphServiceClient;
 @RestController
 public class GraphController {
 	
+    private static String authority;
+    private static String clientId;
+    private static String scope;
+    private static String keyPath;
+    private static String certPath;
+	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		
@@ -55,11 +61,66 @@ public class GraphController {
 		return String.format("Hello %s!", name);
 	}
 	
+	@PostMapping("/users/add")
+	public String addUser() throws IOException {
+		System.out.println("******inside post start******");
+		setUpSampleData();
+		String[] scopesStr = new String[] {".default"};
+		List<String> scopes = Arrays.asList(scopesStr);
+		
+		final OnBehalfOfCredential onBehalfOfCredential = new OnBehalfOfCredentialBuilder()
+		        .clientId("0c4c48ff-aa3d-43af-aabb-4a349cede6ae")
+		        .pemCertificate(certPath)
+		        //.pfxCertificate(certPath)
+		        .tenantId("32af3a3d-6490-4e85-92de-7ed6985aa7ff")
+		        .build();
+
+		final TokenCredentialAuthProvider tokenCredentialAuthProvider = 
+				new TokenCredentialAuthProvider(scopes, onBehalfOfCredential);
+
+		final GraphServiceClient<Request> graphClient =
+		  GraphServiceClient
+		    .builder()
+		    .authenticationProvider(tokenCredentialAuthProvider)
+		    .buildClient();
+		
+		User user = new User();
+		user.accountEnabled = true;
+		user.displayName = "test user";
+		user.mailNickname = "testuser";
+		user.userPrincipalName = "testuser123@jdttestlab1b2c.onmicrosoft.com";
+		com.microsoft.graph.models.PasswordProfile passwordProfile = new com.microsoft.graph.models.PasswordProfile();
+		passwordProfile.password = "TestUser@123";
+		passwordProfile.forceChangePasswordNextSignIn = false;
+		user.passwordProfile = passwordProfile;
+		user.passwordPolicies = "DisablePasswordExpiration";
+		
+		Extension extension = new Extension();
+		extension.additionalDataManager().put("extension_0c4c48ffaa3d43afaabb4a349cede6ae_test", new JsonPrimitive("9988"));
+		
+		graphClient.users().buildRequest().post(user);
+		System.out.println("******inside post end******");
+				
+		return String.format("Hello %s!", "test create user");
+	}
+	
 	@GetMapping("/helloCert")
 	public String helloCert(@RequestParam(value = "name", defaultValue = "World") String name) throws Exception {
 		ClientCredentialGrant clientCredentialGrant = new ClientCredentialGrant();
 		clientCredentialGrant.graphCert();
 		return String.format("Hello %s!", name);
 	}
+	
+	private void setUpSampleData() throws IOException {
+	        // Load properties file and set properties used throughout the sample
+	        Properties properties = new Properties();
+	        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties"));
+	        authority = properties.getProperty("AUTHORITY");
+	        clientId = properties.getProperty("CLIENT_ID");
+	        keyPath = properties.getProperty("KEY_PATH");
+	        certPath = properties.getProperty("CERT_PATH");
+	        scope = properties.getProperty("SCOPE");
+	        System.out.println(keyPath +" : " + certPath);
+	    }
 
 }
